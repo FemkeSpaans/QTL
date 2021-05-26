69a21d13980259899870d32c3dbda40bb70f1ff8
FSpaa <f.spaans@hotmail.nl>
2021-05-26 18:31:34 +0200

    writing to file added

--- Anova.py
+++ Anova.py
@@ -15,7 +15,12 @@ def calculate_anova(list_with_dict):
     :return:
     """
     count = 0
+    anova_list = []
+    file_create = open("anova.txt", "x")
+    file_write = open("anova.txt", "w")
     for marker, dict in list_with_dict.items():
         fvalue, pvalue = scipy.stats.f_oneway(dict['a'], dict['b'])
         count = count + 1
-        print(marker, pvalue, fvalue, count)
+        anova_list = marker, "\t", str(pvalue), "\t", str(fvalue), "\t", str(count), "\n"
+        file_write.writelines(anova_list)
+
